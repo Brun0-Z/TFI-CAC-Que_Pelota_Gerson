8159d6d4a07cc0aa024d8f238dc042e9b595319b
Bruno Zeballos <brunozeballos22@gmail.com>
2023-10-16 22:38:35 -0300

    V4

--- src/togglePopup.js
+++ src/togglePopup.js
@@ -1,3 +1,11 @@
-function togglePopup() {
-    document.getElementById("popup-1").classList.toggle("active");
-}
+const btnAbrirModal = document.querySelector("#btn-abrir-modal");
+const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+const modal = document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+btnCerrarModal.addEventListener("click", () => {
+  modal.close();
+});
